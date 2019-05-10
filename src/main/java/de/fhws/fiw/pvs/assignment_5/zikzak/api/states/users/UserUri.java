6ecafc7b4aed3142fdd2174f663983ee2f4f8a6f
k40216 <maximilian.rehberger@student.fhws.de>
2019-05-11 00:51:33 +0200

    UserUri modified

--- src/main/java/de/fhws/fiw/pvs/assignment_5/zikzak/api/states/users/UserUri.java
+++ src/main/java/de/fhws/fiw/pvs/assignment_5/zikzak/api/states/users/UserUri.java
@@ -21,7 +21,6 @@ import de.fhws.fiw.pvs.assignment_5.zikzak.Start;
 
 public interface UserUri
 {
-
 	String PATH_ELEMENT = "users";
 	String REL_PATH = Start.CONTEXT_PATH + "/api/" + PATH_ELEMENT;
 	String REL_PATH_ID = REL_PATH + "/{id}";
