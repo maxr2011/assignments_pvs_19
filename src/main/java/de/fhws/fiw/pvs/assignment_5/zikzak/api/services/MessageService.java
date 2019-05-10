ea969bfd72657e5b93b373ea5dee73086afb7beb
k40216 <maximilian.rehberger@student.fhws.de>
2019-05-11 00:52:55 +0200

    MessageService created

---
+++ src/main/java/de/fhws/fiw/pvs/assignment_5/zikzak/api/services/MessageService.java
@@ -0,0 +1,58 @@
+package de.fhws.fiw.pvs.assignment_5.zikzak.api.services;
+
+import de.fhws.fiw.pvs.assignment_5.sutton.api.queries.PagingBehaviorUsingOffsetSize;
+import de.fhws.fiw.pvs.assignment_5.sutton.api.services.AbstractService;
+import de.fhws.fiw.pvs.assignment_5.zikzak.api.states.messages.GetAllMessages;
+import de.fhws.fiw.pvs.assignment_5.zikzak.api.states.messages.GetSingleMessage;
+import de.fhws.fiw.pvs.assignment_5.zikzak.api.states.messages.PostNewMessage;
+import de.fhws.fiw.pvs.assignment_5.zikzak.api.states.users.GetAllUsers;
+import de.fhws.fiw.pvs.assignment_5.zikzak.models.MessageModel;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("messages")
+public class MessageService extends AbstractService {
+
+    @GET
+    @Produces( MediaType.APPLICATION_JSON )
+    public Response getAllMessages()
+    {
+        final GetAllMessages.GetAllMessagesQuery query = new GetAllMessages.GetAllMessagesQuery( );
+        query.setPagingBehavior( new PagingBehaviorUsingOffsetSize( 0, 100 ) );
+
+        return new GetAllMessages( ).setQuery( query )
+                .setUriInfo( this.uriInfo )
+                .setRequest( this.request )
+                .setHttpServletRequest( this.httpServletRequest )
+                .setContext( this.context )
+                .build( );
+    }
+
+    @GET
+    @Path( "{id}" )
+    @Produces( MediaType.APPLICATION_JSON )
+    public Response getSingleUsers( @PathParam( "id" ) final long id )
+    {
+        return new GetSingleMessage( ).setRequestedId( id )
+                .setUriInfo( this.uriInfo )
+                .setRequest( this.request )
+                .setHttpServletRequest( this.httpServletRequest )
+                .setContext( this.context )
+                .build( );
+    }
+
+    @POST
+    @Consumes( MediaType.APPLICATION_JSON )
+    public Response createSingleUsers( final MessageModel messageModel )
+    {
+        return new PostNewMessage( ).setModelToStore( messageModel )
+                .setUriInfo( this.uriInfo )
+                .setRequest( this.request )
+                .setHttpServletRequest( this.httpServletRequest )
+                .setContext( this.context )
+                .build( );
+    }
+
+}
