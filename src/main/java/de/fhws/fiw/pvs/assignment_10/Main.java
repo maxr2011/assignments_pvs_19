5a8d76a7b5d603e7520cebb4cf5ef519375f59df
Rehberger, Maximilian <maximilian.rehberger@student.fhws.de>
2019-06-15 00:13:45 +0200

    todo: implement parallel prefix solution

--- src/main/java/de/fhws/fiw/pvs/assignment_10/Main.java
+++ src/main/java/de/fhws/fiw/pvs/assignment_10/Main.java
@@ -1,16 +1,34 @@
 package de.fhws.fiw.pvs.assignment_10;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Main {
+
+    static AtomicInteger atomicInteger = new AtomicInteger(0);
 
     public static void main(String[] args) {
 
         // initialise some values
         int N = 4;
         Integer[] A = {5, 7, 8, 1};
-        Integer[] B = sequentialPrefix(A);
 
-        // print output
+        // measure time here
+        Integer[] B = sequentialPrefix(A);
+        Integer[] P = parallelPrefix(A);
+
+        // print output sequential
+        System.out.println("Sequential: ");
         printOutArray(B);
+
+        System.out.println();
+
+        // print output parallel
+        System.out.println("Parallel: ");
+        printOutArray(P);
 
     }
 
@@ -27,6 +45,19 @@ public class Main {
 
         return output;
 
+    }
+
+    private static Integer[] parallelPrefix(Integer[] input) {
+        // todo implement
+        Integer[] output = new Integer[input.length];
+
+        for(Integer it : input) {
+            new Thread(() -> {
+                // todo implement parallel
+            }).start();
+        }
+
+        return output;
     }
 
     private static void printOutArray(Integer[] arrayPrint) {
